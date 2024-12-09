68356b76136a0de51df3fd04e4500a92f8bb8353
xeen0 <sahitsrivyshnav1999@gmail.com>
2024-12-09 12:50:17 +0530

    TestCase 4: Multiple Input Numbers with ',' delimiter and empty string

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,18 +1,21 @@
 function calculate() {
-  const input = document.getElementById('inputBox').value;
+  const input = document.getElementById("inputBox").value;
   try {
     const result = add(input);
-    document.getElementById('result').textContent = 'Result: ' + result;
+    document.getElementById("result").textContent = "Result: " + result;
   } catch (error) {
-    document.getElementById('result').textContent = error.message;
+    document.getElementById("result").textContent = error.message;
   }
 }
 
 const add = (numbers) => {
-  if(numbers === '') return 0
+  if (numbers === "") return 0;
 
-  const nums = numbers.replace(/\n/g, ',').split(',').map(n =>parseInt(n))
-  return nums.reduce((x,y) => x+y, 0)
-}
+  const nums = numbers
+    .replace(/\n/g, ",")
+    .split(",")
+    .map((n) => (n != "" ? parseInt(n) : 0));
+  return nums.reduce((x, y) => x + y, 0);
+};
 
-module.exports = {add};
+module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,16 +5,19 @@ describe("String Calculator", () => {
     expect(add("")).toBe(0);
   });
 
-  test('should return the number for a single number input', () => {
-    expect(add('1')).toBe(1); 
+  test("should return the number for a single number input", () => {
+    expect(add("1")).toBe(1);
   });
 
-  test('should return the sum of two numbers', () => {
-    expect(add('1,5')).toBe(6);
+  test("should return the sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
   });
 
-  test('should handle new lines between numbers', () => {
-    expect(add('1\n2,3')).toBe(6);
+  test("should return the sum of numbers", () => {
+    expect(add("1,5,,7,5,1,,3")).toBe(22);
   });
-  
+
+  test("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
