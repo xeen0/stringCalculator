2e8adacaf7d162b356ce15d98e660b6499deb2f4
xeen0 <sahitsrivyshnav1999@gmail.com>
2024-12-09 14:49:00 +0530

    Added Count method

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,8 @@
 var totalCount = 0
+
 function calculate() {
   const input = document.getElementById("inputBox").value;
+
   try {
     const result = add(input);
     document.getElementById("result").textContent = "Result: " + result;
@@ -12,6 +14,7 @@ function calculate() {
 window.onload = function() {
   totalCount = 0
   const inputField = document.getElementById("inputBox");
+
   inputField.addEventListener("keydown", function(e) {
     if (e.key === "Enter") {
       e.preventDefault();
@@ -20,33 +23,37 @@ window.onload = function() {
   });
 }
 
-const add = (numbers) => {
-  totalCount++
-  if (numbers === "") return 0;
+const add = (input) => {
+  totalCount++;
 
-  let delimitersRegex = ","
-  numbers = numbers.replace(/\\n/g, '\n');
+  if (input === "") return 0;
 
-  const negativeNumbers = numbers.match(/-\d+/g);
+  let delimitersRegex = ",";
+  input = input.replace(/\\n/g, '\n');
+
+  const negativeNumbers = input.match(/-\d+/g);
   if (negativeNumbers) {
-    throw new Error('negative numbers not allowed: ' + negativeNumbers.join(', '));
+    throw new Error('Negative numbers not allowed: ' + negativeNumbers.join(', '));
   }
-  if (numbers.startsWith('//')) {
-    const delimiterLineEndIndex = numbers.indexOf('\n');
-    let delimiters = numbers.slice(2, delimiterLineEndIndex);
-    let delimitersArray = delimiters.match(/\[(.*?)\]/g).map(d => d.slice(1, -1));
+
+  if (input.startsWith('//')) {
+    const delimiterLineEndIndex = input.indexOf('\n');
+    const delimiters = input.slice(2, delimiterLineEndIndex);
+    const delimitersArray = delimiters.match(/\[(.*?)\]/g).map(d => d.slice(1, -1));
     const escapedDelimiters = delimitersArray.map(d => d.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&'));
     delimitersRegex = new RegExp(escapedDelimiters.join('|'), 'g');
-    numbers = numbers.slice(delimiterLineEndIndex + 1);
+    input = input.slice(delimiterLineEndIndex + 1);
   }
-  const nums = numbers
+
+  const numbers = input
     .replace(/\n/g, ",")
     .split(delimitersRegex)
     .map((n) => {
       const num = parseInt(n);
       return isNaN(num) || num > 1000 ? 0 : num;
-    })
-  return nums.reduce((x, y) => x + y, 0);
+    });
+
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 };
 
 const getCalledCount = () => {
